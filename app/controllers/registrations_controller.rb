7509bcfe6363414503f0b4e1e4267aeb91f1ab30
peddivarunkumar <peddivarunkumar99@gmail.com>
2022-08-08 19:40:30 +0530

    SignUp user

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,19 @@
+class RegistrationsController < ApplicationController
+  def new
+    @user = User.new
+  end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+       redirect_to root_path, notice: "Successfully created account"
+    else
+        
+        render :new
+    end
+  end
+
+  private 
+  def user_params
+   params.require(:user).permit(:name,:password,:password_confirmation,:dob,:photo)
+ end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
   resources :articles do
     resources :comments
   end
+
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to:  "registrations#create"
 end
