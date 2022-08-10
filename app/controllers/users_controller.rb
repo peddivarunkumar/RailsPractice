bc18597fa12681a4a8c2400106855bd5ccf46216
peddivarunkumar <peddivarunkumar99@gmail.com>
2022-08-10 10:43:33 +0530

    Allow only admin users to access users paths

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :is_Admin
+
   def index
     @users = User.all
   end
@@ -27,6 +29,14 @@ class UsersController < ApplicationController
   end
 
   private
+    def is_Admin
+       if session[:user_id] and Current.user.admin_user
+        
+       else
+        redirect_to articles_path
+       end
+    end 
+
     def user_params
       params.require(:user).permit(:name,:dob,:photo)
     end
