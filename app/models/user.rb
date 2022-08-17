a0638b5b984600da8975d60d941cbf7c9dfe7705
peddivarunkumar <peddivarunkumar99@gmail.com>
2022-08-17 15:42:10 +0530

    Fix error when a user is deleted

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
     has_secure_password
     mount_uploader :photo, PhotoUploader
-    
+
+    has_many :articles, dependent: :destroy
+
     validates :name, presence: true, uniqueness: true
 end
