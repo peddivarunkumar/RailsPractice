2ac7fe770060c2eab244c798399d46d1570227b3
peddivarunkumar <peddivarunkumar99@gmail.com>
2022-08-23 16:49:28 +0530

    Do nothing when a user is already registered for the meeting

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,8 +4,6 @@ class BookingsController < ApplicationController
       event_id = params[:event_id].to_i
       if !@user.events.find(event_id)
         @user.events << Event.find(params[:event_id].to_i)
-      else
-        puts "Already registered"
       end
     end
 end
