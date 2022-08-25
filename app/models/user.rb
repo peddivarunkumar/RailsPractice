eccd05de3a3f3da0f9b7489eb5a2645bc322ccd0
peddivarunkumar <peddivarunkumar99@gmail.com>
2022-08-25 11:55:50 +0530

    Minor fixes app works fine

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,10 @@ class User < ApplicationRecord
     has_secure_password
 
     has_many :articles, dependent: :destroy
-    has_many :bookings
-    has_many :events, through: :bookings
-    has_one :picture, as: :imageable
+    has_many :bookings, dependent: :destroy
+    has_many :events, through: :bookings, dependent: :destroy
+    has_one :picture, as: :imageable, dependent: :destroy
+    has_many :comments, dependent: :destroy
 
     accepts_nested_attributes_for :picture, update_only: true
 
